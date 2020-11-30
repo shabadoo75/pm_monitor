6f7b2f16ed6c4cd32e109085fdbd6fa7c71827f6
Joe Lipson <joe.lipson@abc.net.au>
2020-11-30 18:11:55 +1100

    ask about a display

--- install.sh
+++ install.sh
@@ -10,7 +10,13 @@ sudo apt-get -y install ansible
 echo
 echo
 
+echo "Now we need some configuration details for your particle monitor"
+echo
+
 read -e -p "Hostname for the Pi: " -i "pm_monitor" hostname
+
+read -e -p "Do you have a Adafruit PiOLED display?: " -i "N" display
+display=$(echo $display | tr [A-Z] [a-z])
 
 read -e -p "datadog API key: " -i "" dd_api_key
 
@@ -21,13 +27,14 @@ echo
 echo "****************************"
 echo "Configuration values:"
 echo "Hostname: $hostname"
+echo "Display: $display"
 echo "datadog API key: $dd_api_key"
 echo "datadog app key: $dd_app_key"
 echo "****************************"
 echo
 
-read -e -p "Looks good, start the install? [Y/N]: " -i "N" install
-if [ "$install" = "Y" ]; then
+read -e -p "Look good? start the install [Y/N]: " -i "N" install
+if [[ "$install" = "Y" || "$install" = "y" ]]; then
   echo "starting..."
 else
   echo "exit"
@@ -38,4 +45,5 @@ ansible-playbook -u pi \
     -e "hostname=$hostname" \
     -e "DD_API_KEY=$dd_api_key" \
     -e "DD_APP_KEY=$dd_app_key" \
+    -e "display=$display" \
     ansible/playbook.yml
